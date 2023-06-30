cbcabdb83116766fa7a4a4ce7bdc4c788afafdd3
smendes <sergio.coti@gmail.com>
2023-06-30 12:14:44 -0300

    feat: Desenvolver os fluxos de edição, exclusão e consulta de clientes no controlador

--- Projeto06/Controllers/ClienteController.cs
+++ Projeto06/Controllers/ClienteController.cs
@@ -27,12 +27,25 @@ namespace Projeto06.Controllers
                 switch(opcao)
                 {
                     case 1: CadastrarCliente(); break;
-                    case 2: break;
-                    case 3: break;
-                    case 4: break;
+                    case 2: AtualizarCliente(); break;
+                    case 3: ExcluirCliente(); break;
+                    case 4: ConsultarClientes(); break;
                     default:
                         Console.WriteLine("\nOPÇÃO INVÁLIDA.");
                         break;
+                }
+
+                Console.Write("\nDESEJA CONTINUAR? (S,N): ");
+                var escolha = Console.ReadLine();
+
+                if(escolha.Equals("S", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.Clear(); //limpar o prompt do DOS
+                    ExecutarMenu(); //recursividade!
+                }
+                else
+                {
+                    Console.WriteLine("\nFIM DO PROGRAMA!");
                 }
             }
             catch(Exception e)
@@ -72,6 +85,116 @@ namespace Projeto06.Controllers
             catch(Exception e)
             {
                 Console.WriteLine("\nFalha ao cadastrar cliente:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        //método para executar a edição de um cliente
+        public void AtualizarCliente()
+        {
+            try
+            {
+                Console.WriteLine("\n*** EDIÇÃO DE CLIENTE ***\n");
+
+                Console.Write("INFORME O ID DO CLIENTE....: ");
+                var id = Guid.Parse(Console.ReadLine());
+
+                //pesquisar no banco de dados o cliente através do id
+                var clienteRepository = new ClienteRepository();
+                var cliente = clienteRepository.ObterPorId(id);
+
+                //verificar se o cliente foi encontrado
+                if(cliente != null)
+                {
+                    Console.Write("INFORME O NOME DO CLIENTE..: ");
+                    cliente.Nome = Console.ReadLine();
+
+                    Console.Write("INFORME O CPF DO CLIENTE...: ");
+                    cliente.Cpf = Console.ReadLine();
+
+                    Console.Write("INFORME A DATA DE NASC.....: ");
+                    cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+                    //atualizando o cliente no banco de dados
+                    clienteRepository.Atualizar(cliente);
+
+                    Console.WriteLine("\nCLIENTE ATUALIZADO COM SUCESSO.");
+                }
+                else
+                {
+                    Console.WriteLine("\nCliente não encontrado.");
+                }
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine("\nOcorreram erros de validação.");
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nFalha ao atualizar cliente:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        //método para executar a exclusão de um cliente
+        public void ExcluirCliente()
+        {
+            try
+            {
+                Console.WriteLine("\n*** EXCLUSÃO DE CLIENTE ***\n");
+
+                Console.Write("INFORME O ID DO CLIENTE....: ");
+                var id = Guid.Parse(Console.ReadLine());
+
+                //pesquisar o cliente no banco de dados através do id
+                var clienteRepository = new ClienteRepository();
+                var cliente = clienteRepository.ObterPorId(id);
+
+                //verificar se o cliente foi encontrado
+                if(cliente != null)
+                {
+                    //excluindo o cliente
+                    clienteRepository.Excluir(cliente);
+
+                    Console.WriteLine("\nCLIENTE EXCLUÍDO COM SUCESSO.");
+                }
+                else
+                {
+                    Console.WriteLine("\nCliente não encontrado.");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nFalha ao excluir cliente:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        //método para consultar e imprimir todos os clientes cadastrados
+        public void ConsultarClientes()
+        {
+            try
+            {
+                Console.WriteLine("\n*** CONSULTA DE CLIENTES ***\n");
+
+                //consultando todos os clientes cadastrados no banco de dados
+                var clienteRepository = new ClienteRepository();
+                var clientes = clienteRepository.ObterTodos();
+
+                //varrer e imprimir a lista de clientes:
+                foreach (var item in clientes)
+                {
+                    Console.WriteLine("ID...........: " + item.Id);
+                    Console.WriteLine("NOME.........: " + item.Nome);
+                    Console.WriteLine("CPF..........: " + item.Cpf);
+                    Console.WriteLine("DATA DE NASC.: " + item.DataNascimento);
+                    Console.WriteLine("...");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nFalha ao consultar clientes.");
                 Console.WriteLine(e.Message);
             }
         }
